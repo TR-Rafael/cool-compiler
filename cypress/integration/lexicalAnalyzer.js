56821c888d17d4b243af0a5e21053749c26276b8
Rafael-TR <silva_rafael@id.uff.br>
2022-09-06 17:25:30 -0300

    feat: add log to improve visualization

--- cypress/integration/lexicalAnalyzer.js
+++ cypress/integration/lexicalAnalyzer.js
@@ -4,10 +4,11 @@ const title = 'Lexical analyzer'
 // change the value of the `pathForFileInCOOL ` variable to the path of the file you want to compile.
 // EX: 'cypress/fixtures/examples/helloWorld.cl'
 
-const pathForFileInCOOL = 'cypress/fixtures/examples/complex.cl'
+const pathForFileInCOOL = 'cypress/fixtures/examples/bookList.cl'
 describe(title, () => {
   it(title, () => {
     cy.readFile(pathForFileInCOOL).then(codeRaw => {
+      console.log(codeRaw)
       const tokens = lexiconAnalyzer({ codeRaw })
       console.log(tokens)
     })
